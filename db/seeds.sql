a811a99385e0860d5f74b8bd4c972aa79d52677f
PeeWee <oscar.oses.1127@gmail.com>
2020-09-14 13:11:22 -0400

    added initial files, created schema and seeds, added config folder with connections and modules

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Colossal Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Taco Burger', true);
